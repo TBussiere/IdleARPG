1858fbae7ca26f0be0d18a3e2801ebe08479ffe8
TB <lewho38@gmail.com>
2020-06-13 17:29:01 +0200

    Responsive design

---
+++ Assets/scripts/UIContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIContainer : MonoBehaviour
+{
+    public RectTransform rectTransform;
+    public GameObject prefabUI;
+    private UISeparator uISeparator1;
+    private UISeparator uISeparator2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float size = this.GetComponentInParent<GraphicRaycaster>().GetComponent<RectTransform>().rect.height;
+        Debug.Log(size);
+        rectTransform.sizeDelta = new Vector2(0, size * 0.375f);
+        rectTransform.anchoredPosition = new Vector2(0, (size * 0.375f) / 2);
+    }
+
+}
---
+++ Assets/scripts/UISeparator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+public class UISeparator : MonoBehaviour
+{
+    public int nbperso;
+    public int id;
+    public RectTransform parentobjct;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float tailleParent = parentobjct.rect.width;
+
+        float posX = tailleParent / nbperso * (id + 1);
+
+        this.GetComponent<RectTransform>().anchoredPosition = new Vector2(posX, 0);
+    }
+}
